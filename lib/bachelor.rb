821d328dd38b9d3f9e772f6e702fa9ec88584055
Daniel Walz <danielwalz@Daniels-MacBook-Pro.local>
2018-08-12 07:17:49 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,42 @@
+require"pry"
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].find {|x| x["status"] == "Winner"}["name"].split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.map do |season, contestants|
+    contestants.find do |person_info|
+      person_info ["occupation"] == occupation
+    end
+  end.compact.first["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  same_home = []
+  data.each do |season, contestants|
+    contestants.each do |person_info|
+      if person_info["hometown"] == hometown
+        same_home << person_info["hometown"]
+      end
+    end
+  end
+  same_home.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.map do |season, contestants|
+    contestants.find do |person_info|
+      person_info ["hometown"] == hometown
+    end
+  end.compact.first["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  container_of_ages = []
+  data[season].each do |person_info|
+    container_of_ages << person_info["age"].to_i
+  end
+  x = container_of_ages.inject{|sum, x| sum + x}.to_f / container_of_ages.size
+  x.round
 end
